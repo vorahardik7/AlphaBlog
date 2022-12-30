518ff99b3a55de8f32634542aee6afe65790523e
vorahardik7 <vorahardik056@gmail.com>
2022-12-30 10:48:39 +0530

    user validations added with email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+    validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3 , maximum: 25 }
+    VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, uniqueness: { case_sensitive: false }, presence: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_FORMAT }
 end
