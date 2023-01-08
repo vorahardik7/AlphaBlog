0952d302880ecab621e2e19921b4170e87bb2a66
vorahardik7 <vorahardik056@gmail.com>
2023-01-08 10:37:14 +0530

    added category and integration tests for admin users only

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:show, :index]
+    
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category
+        else
+            render "new"
+        end
+    end
+
+    def index
+        @categories = Category.paginate(page: params[:page], per_page: 3)
+    end
+
+    def show
+        @category = Category.find(params[:id])
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "You do not have admin access"
+            redirect_to categories_path
+        end
+    end
+end
---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+      password: "password", admin: true)
+      sign_in_as(@admin_user)
+  end
+  
+  test "get category form and create category" do
+    get "/categories/new"
+    assert_response :success
+    assert_difference('Category.count', 1) do
+      post categories_path, params: { category: { name: "Sports" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+  
+  test "get category form and reject invalid category category" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference('Category.count') do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "Errors", response.body
+    assert_select 'div.alert'
+
+  end
+end
